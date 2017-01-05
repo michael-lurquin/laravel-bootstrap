c5fbbbf824a71f78ff2d3e7b3f94102a51c05b78
Michaël Lurquin <michael.lurquin@gmail.com>
2017-01-05 17:03:04 +0100

    Add config package

---
+++ src/config/laravel-bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'bootstrap-version' => '3.3.7',
+    'default_values' => [
+        'button' => [
+            'label' => '',
+            'class' => 'default',
+            'glyph' => '',
+        ],
+    ],
+];
