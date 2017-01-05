2e00be0fbdbeb2519c6cd05a0aa611e4b947a191
Michaël Lurquin <michael.lurquin@gmail.com>
2017-01-05 17:03:52 +0100

    Fix generate HTML

--- src/Traits/BladeTwitterBootstrap.php
+++ src/Traits/BladeTwitterBootstrap.php
@@ -20,14 +20,16 @@ trait BladeTwitterBootstrap
     /**
      * Generate a "button" HTML tag
      *
-     * @param  [string] $value [Button label]
+     * @param  [string] $label [Button label]
      * @param  [string] $class [Button style (default, primary, success, info, warning, danger)]
+     * @param  [string] $glyph [Name of the glyphicon (without the prefix 'glyphicon glyphicon-')]
      * @return [string]
      */
     public function button($label, $class, $glyph)
     {
-        $label = is_null($label) ? '' : ' ' . $label;
-        $output = !is_null($glyph) ? $this->glyph($glyph) . $label : $label;
+        $label = empty($label) ? '' : $label;
+        $label = !empty($label) && !empty($glyph) ? ' ' . $label : $label;
+        $output = !empty($glyph) ? $this->glyph($glyph) . $label : $label;
 
         return "<button type='button' class='btn btn-{$class}'>{$output}</button>";
     }
